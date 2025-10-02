8f4b4f5f96235c1416bd63abb18f7f7111ce4575
Marlon Quinde <mquinde@viamatica.com>
2025-10-01 22:53:42 -0500

    feat(CardMovie): Estilo en card movie

--- src/app/modules/movie/components/movie-card/movie-card.ts
+++ src/app/modules/movie/components/movie-card/movie-card.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { IMovie } from '../../interfaces/IMovieNowPlaying.interface';
+import { environments } from '../../../../environments/environments';
 
 @Component({
   selector: 'app-movie-card',
@@ -7,7 +8,20 @@ import { IMovie } from '../../interfaces/IMovieNowPlaying.interface';
   templateUrl: './movie-card.html',
   styleUrl: './movie-card.scss'
 })
-export class MovieCard {
+export class MovieCard implements OnInit, AfterViewInit{
+
+  @ViewChild('movieCard') movieCard?: ElementRef<HTMLDivElement>
 
   @Input({ required: true}) movie!: IMovie
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(){
+    if(this.movieCard){
+      this.movieCard.nativeElement.style.backgroundImage = `url("${environments.baseUrlTMDB_IMAGE}${this.movie.poster_path}")`;
+      this.movieCard.nativeElement.style.backgroundSize = 'cover'
+      this.movieCard.nativeElement.style.setProperty('--text-card-movie', `'${this.movie.title}`)
+    }
+  }
 }
